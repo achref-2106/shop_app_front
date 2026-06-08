0695955920dd803df8f43440be0e49437cd7bc35
achref-21 <achref.cha21rgui@gmail.com>
2026-06-08 13:00:49 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/shop-pwa/sw.js', { scope: '/shop-pwa/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/shop_app_front/sw.js', { scope: '/shop_app_front/' })})}
